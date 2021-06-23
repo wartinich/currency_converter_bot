adc0dd517508ed28435daa012cf84259f9d30715
Arkhip Vasilenko <vasilenko.arhip@gmail.com>
2021-06-23 14:13:01 +0300

    Add strange scramper

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-from typing import Text
 from moduls.config import TOKEN
-from moduls.scraper import btc_rate, title
+from moduls.scraper import *
 import telebot
+
 
 
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, f'Здраствуйте!\nКурс криптовалют - /crypto_rate\nКурс валют - /money_rate ' )
+	bot.send_message(message.chat.id, f'Здраствуйте!\nКурс криптовалют - /crypto_rate\nКурс валют - /money_rate ')
+
 
 @bot.message_handler(commands=['crypto_rate'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, f'Курс {title}: {btc_rate}')
-
-
-# @bot.message_handler(commands=['money_rate'])
-# def send_welcome(message):
-# 	bot.send_message(message.chat.id, f'Курс доллара: {money_rate}')
-
-
-
+	bot.send_message(message.chat.id, crypto_rating)
 
 if __name__ == "__main__":
     print('The bot is running!')
--- moduls/scraper.py
+++ moduls/scraper.py
@@ -1,21 +1,45 @@
-
-from sys import flags
 from bs4 import BeautifulSoup
 import requests
 import lxml
-import math
 
 
-url = 'https://coinmarketcap.com/ru/currencies/bitcoin/'
-response = requests.get(url)
+#BTC rate
+url_bitcoin = 'https://coinmarketcap.com/currencies/bitcoin/'
+response_bitcoin= requests.get(url_bitcoin)
+soup_bitcoin = BeautifulSoup(response_bitcoin.text, 'lxml')
+bitcoin_rate = soup_bitcoin.find('div', class_='priceValue___11gHJ').text
+main_bitcoin_rate = "".join(bitcoin_rate.split(","))
+title_bitcoin = soup_bitcoin.find('h2', class_="sc-1q9q90x-0 iYFMbU h1___3QSYG").find('small', class_="nameSymbol___1arQV").text
+print(f'Курс {title_bitcoin}:{main_bitcoin_rate}')
 
-soup = BeautifulSoup(response.text, 'lxml')
+#ETC rate
+url_ethereum = 'https://coinmarketcap.com/currencies/ethereum/'
+response_ethereum = requests.get(url_ethereum)
+soup_ethereum= BeautifulSoup(response_ethereum.text, 'lxml')
+ethereum_rate = soup_ethereum.find('div', class_='priceValue___11gHJ').text
+main_ethereum_rate = "".join(ethereum_rate.split(","))
+title_ethereum = soup_ethereum.find('h2', class_="sc-1q9q90x-0 iYFMbU h1___3QSYG").find('small', class_="nameSymbol___1arQV").text
+print(f'Курс {title_ethereum}:{main_ethereum_rate}')
 
+#XRP rate
+url_xrp = 'https://coinmarketcap.com/currencies/xrp/'
+response_xrp = requests.get(url_xrp)
+soup_xrp = BeautifulSoup(response_xrp.text, 'lxml')
+xrp_rate = soup_xrp.find('div', class_='priceValue___11gHJ').text
+main_xrp_rate = "".join(xrp_rate.split(","))
+title_xrp = soup_xrp.find('h2', class_="sc-1q9q90x-0 iYFMbU h1___3QSYG").find('small', class_="nameSymbol___1arQV").text
+print(f'Курс {title_xrp}:{main_xrp_rate}')
 
-btc_rate = soup.find('div', class_='priceValue___11gHJ').text.split()
-title = soup.find('h1', class_="priceHeading___2GB9O").find('small', recursive=False)
+#ADA(cardano) rate
+url_cardano = 'https://coinmarketcap.com/currencies/cardano/'
+response_cardano = requests.get(url_cardano)
+soup_cardano = BeautifulSoup(response_cardano.text, 'lxml')
+cardano_rate = soup_cardano.find('div', class_='priceValue___11gHJ').text
+main_cardano_rate = "".join(cardano_rate.split(","))
+title_cardano = soup_cardano.find('h2', class_="sc-1q9q90x-0 iYFMbU h1___3QSYG").find('small', class_="nameSymbol___1arQV").text
+print(f'Курс {title_cardano}:{main_cardano_rate}')
 
-print(btc_rate, title.text)
+crypto_rating = f'Курс {title_bitcoin} : {main_bitcoin_rate}\nКурс {title_ethereum} : {main_ethereum_rate}\nКурс {title_xrp} : {main_xrp_rate}\nКурс {title_cardano} : {main_cardano_rate}'
 
 
 
